f79eeae356a35b4fe410aaffd93aeada52086eb9
NilBrillarelliiIcra <nilbrillaicra@gmail.com>
2026-02-06 21:26:39 +0100

    Tileset + Update PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,42 +5,21 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     private float Speed = 5.0f;
-
-    Rigidbody2D rigidbody;
-    private bool isMoving;
-
-    private int score = 2000;
-
-    public bool IsMoving => isMoving;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
 
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    public void OnMove(InputValue value)
+    void Update()
     {
-        // Read value from control, the type depends on what
-        // type of controls the action is bound to
-        var moveDir = value.Get<Vector2>();
-
-        Vector2 velocity = moveDir * Speed;
-        rigidbody.linearVelocity = velocity;
-
-        isMoving = (velocity.magnitude > 0.01f);
-
-        if (isMoving)
-        {
-            float angle = Mathf.Atan2(moveDir.y, moveDir.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0, 0, angle);
-        }
+        rb.linearVelocity = moveInput * Speed;
     }
 
-    // NOTE: InputSystem: "SaveScore" action becomes "OnSaveScore" method
-    public void OnSaveScore()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        // Usage example on how to save score
-        PlayerPrefs.SetInt("Score", score);
-        score = PlayerPrefs.GetInt("Score");
+        moveInput = context.ReadValue<Vector2>();
     }
 }
